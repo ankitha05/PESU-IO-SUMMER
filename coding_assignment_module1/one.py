c6d9b1674f5895e50b35c8cea79cf93bdf137e6e
ankitha05 <51334387+ankitha05@users.noreply.github.com>
2019-06-09 22:07:52 +0530

    Update one.py

--- coding_assignment_module1/one.py
+++ coding_assignment_module1/one.py
@@ -1,1 +1,8 @@
-
+values = input("Input comma seprated numbers : ")
+list1 = values.split(",")
+list = []
+for i in list1:
+	list.append(int(i))
+tuple = tuple(list)
+print('List : ',list)
+print('Tuple : ',tuple)
